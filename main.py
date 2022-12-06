c4a3fde38d3cfd131e62d2c4d5c6a7b544c62a00
snake <admin@viasnake.com>
2022-12-06 20:13:35 +0900

    Improve

--- main.py
+++ main.py
@@ -52,34 +52,63 @@ async def on_ready():
 
 @bot.command()
 async def ai(ctx, *, prompt):
-    print("prompt: " + prompt)
+    await ctx.message.add_reaction('👀')
+    print('Prompt: ' + prompt)
+    prompt = await add_period(prompt)
+    print('Modified Prompt: ' + prompt)
     async with aiohttp.ClientSession() as session:
-        async with session.post(
-            'https://api.openai.com/v1/completions',
-            headers={
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY']
-            },
-            json={
-                'model': 'text-davinci-003',
-                'prompt': prompt,
-                'max_tokens': 512,
-                'temperature': 0.9,
-                'top_p': 1,
-                'n': 1
-            }
-        ) as response:
-            reply = "No reply"
-            if response.status == 200:
-                data = await response.json()
-                reply = data['choices'][0]['text']
-                try:
-                    await ctx.reply(reply)
-                except:
-                    await ctx.send('エラーが発生しました')
-            else:
-                reply = "Error: " + str(response.status)
-                await ctx.reply('エラー: {}'.format(response.status))
-            await write_csv(prompt, reply)
+        async with ctx.typing():
+            async with session.post(
+                'https://api.openai.com/v1/completions',
+                headers={
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY']
+                },
+                json={
+                    'model': 'text-davinci-003',
+                    'prompt': prompt,
+                    'max_tokens': 512,
+                    'temperature': 0.9,
+                    'top_p': 1,
+                    'n': 1
+                }
+            ) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    reply = [
+                        data['created'],
+                        data['model'],
+                        prompt,
+                        data['choices'][0]['text'],
+                        data['choices'][0]['finish_reason'],
+                        data['usage']['prompt_tokens'],
+                        data['usage']['completion_tokens'],
+                        data['usage']['total_tokens']
+                    ]
+                    try:
+                        await ctx.reply(reply[3])
+                        await ctx.message.remove_reaction('👀', bot.user)
+                        await ctx.message.add_reaction('✅')
+                    except:
+                        await ctx.send(
+                            '{ctx.author.mention}',
+                            '**質問:**',
+                            '{reply[2]}',
+                            '**回答:**',
+                            '{reply[3]}'
+                        )
+                    print('Reply: ' + reply[3])
+                else:
+                    reply = 'Error: ' + str(response.status)
+                    try:
+                        await ctx.reply(reply)
+                        await ctx.message.remove_reaction('👀', bot.user)
+                        await ctx.message.add_reaction('❌')
+                    except:
+                        await ctx.send(ctx.author.mention + reply)
+                    print('Reply: ' + reply)
+    await write_csv(reply)
+
+
 
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
